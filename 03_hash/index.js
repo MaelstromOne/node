e230bd6506f92550369d9ee5db71a483c5a206e9
Albert <albertxgm@gmail.com>
2021-08-06 07:41:35 +0300

    Рефакторинг через Promise

--- 03_hash/index.js
+++ 03_hash/index.js
@@ -6,52 +6,53 @@ if (process.argv.length < 3) {
   return;
 }
 
-const url = process.argv[2];
-const urlHash = url + ".sha256";
-const remoteUrl = url.includes("http");
+checkHash(process.argv[2]);
 
-if (remoteUrl) {
-  fetch(url)
-    .then((res) => {
-      if (res.status === 404) {
-        throw "Not found file";
+function checkHash(url) {
+  const urlHash = url + ".sha256";
+  const remoteUrl = url.includes("http");
+  const getHashes = remoteUrl ? getHashesFromRemoteUrl : getHashesFromLocaleUrl;
+
+  getHashes(url, urlHash)
+    .then((hash) => {
+      if (hash[0] === hash[1]) {
+        console.log("Checksum was match");
+      } else {
+        process.exit(102);
       }
-      return res.buffer();
     })
-    .then((data) => crypto.update(data).digest("hex"))
-    .catch(() => process.exit(100))
-    .then((hash) =>
-      fetch(urlHash)
-        .then((res) => {
-          if (res.status === 404) {
-            throw "Not found file";
-          }
-          return res.text();
-        })
-        .then((data) => {
-          if (hash === data.trim()) {
-            console.log("Checksum was match");
-          } else {
-            process.exit(102);
-          }
-        })
-    )
-    .catch(() => process.exit(101));
-} else {
-  fs.readFile(url)
-    .then((data) => crypto.update(data).digest("hex"))
-    .catch(() => process.exit(100))
-    .then((hash) =>
-      fs
-        .readFile(urlHash)
-        .then((data) => data.toString())
-        .then((data) => {
-          if (hash === data.trim()) {
-            console.log("Checksum was match");
-          } else {
-            process.exit(102);
-          }
-        })
-    )
-    .catch(() => process.exit(101));
+}
+
+async function getHashesFromRemoteUrl(url, urlHash) {
+  return await Promise.all([
+    fetch(url)
+      .then((res) => {
+        if (res.status === 404) {
+          throw "Not found file";
+        }
+        return res.buffer();
+      })
+      .then((data) => crypto.update(data).digest("hex"))
+      .catch(() => process.exit(100)),
+    fetch(urlHash)
+      .then((res) => {
+        if (res.status === 404) {
+          throw "Not found file";
+        }
+        return res.text();
+      })
+      .then(text => text.trim())
+      .catch(() => process.exit(101))
+  ])
+}
+
+async function getHashesFromLocaleUrl(url, urlHash) {
+  return await Promise.all([
+    fs.readFile(url)
+      .then((data) => crypto.update(data).digest("hex"))
+      .catch(() => process.exit(100)),
+    fs.readFile(urlHash)
+      .then((data) => data.toString().trim())
+      .catch(() => process.exit(101))
+  ])
 }
