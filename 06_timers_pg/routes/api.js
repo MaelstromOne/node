3e905775fe699ccf169cc6f34bcad21200fcf867
Albert Gabbasov <albertxgm@gmail.com>
2021-09-12 03:11:32 +0300

    Рефакторинг

--- 06_timers_pg/routes/api.js
+++ 06_timers_pg/routes/api.js
@@ -1,44 +1,7 @@
 const express = require("express");
-const { client, connection } = require("./knexfile");
+const { createTimer, getTimers, findTimerById, updateTimer } = require("../utils/db");
 
 const router = express.Router();
-const knex = require("knex")({
-  client,
-  connection,
-});
-
-const createTimer = (user_id, description) => {
-  return knex("timers")
-    .insert({
-      user_id,
-      start: new Date().toISOString(),
-      description: description,
-      is_active: true,
-    })
-    .returning("id");
-};
-
-const getTimers = (user_id, isActive) => {
-  return knex("timers").select().where({ user_id, is_active: isActive });
-};
-
-const findTimerById = (id) => {
-  return knex("timers")
-    .select()
-    .where({ id })
-    .limit(1)
-    .then((results) => results[0]);
-};
-
-const updateTimer = async (timer) => {
-  await knex("timers")
-    .update({
-      is_active: timer.is_active,
-      duration: timer.duration,
-      end: timer.end,
-    })
-    .where({ id: timer.id });
-};
 
 router.get("/", async (req, res) => {
   const timers = await getTimers((await req.user).id, req.query.isActive);
