a2c58e1b069daee48d70ac484830c42bd6591786
Albert <albertxgm@gmail.com>
2021-10-22 14:50:29 +0300

    Перенесен websocket в отдельный файл

--- 10_ws/index.js
+++ 10_ws/index.js
@@ -3,9 +3,8 @@ const nunjucks = require("nunjucks");
 const cookieParser = require("cookie-parser");
 const cookie = require("cookie");
 const { auth } = require("./utils/middleware");
-const ws = require("ws");
 const { findUserBySessionId } = require("./utils/db");
-const { createTimer, getTimers, findTimerById, updateTimer } = require("./utils/db");
+const wsServer = require("./routes/websocket");
 
 const app = express();
 
@@ -30,65 +29,6 @@ app.use(cookieParser());
 app.use(auth);
 
 app.use("/", require("./routes/root"));
-app.use("/api/timers", require("./routes/api"));
-
-const wsServer = new ws.Server({ noServer: true });
-
-wsServer.on("connection", (socket, req) => {
-  socket.on("message", async (message) => {
-    let data;
-    try {
-      data = JSON.parse(message);
-    } catch (e) {
-      console.log(e);
-    }
-    switch (data.type) {
-      case "fetchTimers": {
-        const timers = await getTimers(req.user.id, data.isActive);
-        if (data.isActive) {
-          timers.forEach((el) => {
-            el.progress = new Date() - el.start;
-          });
-        }
-        socket.send(
-          JSON.stringify({
-            type: data.isActive ? "activeTimers" : "oldTimers",
-            timers,
-          })
-        );
-        break;
-      }
-      case "createTimer": {
-        const id = await createTimer(req.user.id, data.description);
-        socket.send(
-          JSON.stringify({
-            type: "createTimer",
-            id,
-            description: data.description,
-          })
-        );
-        break;
-      }
-      case "stopTimer": {
-        const timer = await findTimerById(data.id);
-        const end = new Date();
-        timer.is_active = false;
-        timer.end = end.toISOString();
-        timer.duration = end - timer.start;
-        timer.start = timer.start.toISOString();
-        await updateTimer(timer);
-
-        socket.send(
-          JSON.stringify({
-            type: "stopTimer",
-            id: timer.id,
-          })
-        );
-        break;
-      }
-    }
-  });
-});
 
 const port = process.env.PORT || 3000;
 
